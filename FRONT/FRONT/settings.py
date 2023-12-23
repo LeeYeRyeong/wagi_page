5fab1b4b538a8b0d036366961547c0563f7d208a
sujin <csujin2040@gmail.com>
2023-12-23 22:53:34 +0900

    노력

--- FRONT/FRONT/settings.py
+++ FRONT/FRONT/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'FRONT',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'FRONT.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR , 'templates'],
+        'DIRS': [os.path.join(BASE_DIR , 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,9 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+import os
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "FRONT", "static")
+    os.path.join(BASE_DIR, 'FRONT/FRONT', 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
---
+++ FRONT/wagiapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WagiappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'wagiapp'
