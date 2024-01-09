d69d3eeec07e53bbd336a82629aab440ce7e9063
romdy <kimjh0937@gmail.com>
2024-01-10 02:22:48 +0900

    지원파트 백엔드

--- BACKEND/homepage/joinInfo/admin.py
+++ BACKEND/homepage/joinInfo/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import JoinInfo
 
-admin.site.register(JoinInfo)
-
 # Register your models here.
+
+@admin.register(JoinInfo)
+class JoinInfoAdmin(admin.ModelAdmin):
+    list_display = ['user_name', 'user_number', 'user_major', 'user_phone_number']
