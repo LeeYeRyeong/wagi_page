c348f6502da40ca0f75fdfed2c11dc9f182141ca
Lee <i876838@gmail.com>
2024-01-09 20:43:57 +0900

    people 최종 수정 완료

--- BACKEND/homepage/homepage/urls.py
+++ BACKEND/homepage/homepage/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from joinInfo.views import apply_club_user_info_view
 from activity.views import upload_activity, success_page, edit_page, edit_images
+from people.views import show_people
 from joinResult import views
 
 urlpatterns = [
@@ -28,5 +29,6 @@ urlpatterns = [
     path('upload/', upload_activity, name='upload_activity'),
     path('success/', success_page, name='success_page'),
     path('edit/', edit_page, name='edit_page'),
-    path('edit_images/<int:image_id>/', edit_images, name='edit_images'),  
+    path('edit_images/<int:image_id>/', edit_images, name='edit_images'),
+    path('people/', show_people, name='show_people'),
 ]
