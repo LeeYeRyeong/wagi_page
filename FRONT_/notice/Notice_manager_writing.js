020a2391968843106ed5a2fe7395bfebafe78c52
Suri <quffl2002@naver.com>
2024-01-10 18:08:37 +0900

    수정본

--- FRONT_/notice/Notice_manager_writing.js
+++ FRONT_/notice/Notice_manager_writing.js
@@ -1,23 +1,23 @@
-document.getElementById("signUp").addEventListener("click", function(event) {
-    event.preventDefault();
-    window.location.href = "Notice_manager_record.html";
-});
-
 document.getElementById("regularActivity").addEventListener("click", function() {
-    document.getElementById("titleWriting").value = "정기활동 제목을 입력해주세요";
-    document.getElementById("contentsWriting").value = "정기활동 내용을 입력해주세요";
+    updateButtonStyle("regularActivity");
 });
 
 document.getElementById("event").addEventListener("click", function() {
-    document.getElementById("titleWriting").value = "행사 제목을 입력해주세요";
-    document.getElementById("contentsWriting").value = "행사 내용을 입력해주세요";
+    updateButtonStyle("event");
 });
 
 document.getElementById("competition").addEventListener("click", function() {
-    document.getElementById("titleWriting").value = "공모전 제목을 입력해주세요";
-    document.getElementById("contentsWriting").value = "공모전 내용을 입력해주세요";
+    updateButtonStyle("competition");
 });
 
-document.getElementById("addPhotoLabel").addEventListener("click", function() {
-    document.getElementById("profile").click();
-});
+function updateButtonStyle(activeButton) {
+    var buttons = document.querySelectorAll('.round-button');
+    buttons.forEach(function(button) {
+        button.classList.remove('selected');
+    });
+
+    var clickedButton = document.getElementById(activeButton);
+    if (clickedButton) {
+        clickedButton.classList.add('selected');
+    }
+}
