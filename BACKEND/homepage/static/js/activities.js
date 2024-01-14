a99eb7b919635acaf82b6c77b8c3541b13654295
rangrang <lunaticmoon1113@naver.com>
2024-01-15 00:59:03 +0900

    파일명 나옴

--- BACKEND/homepage/static/js/activities.js
+++ BACKEND/homepage/static/js/activities.js
@@ -556,39 +556,28 @@ window.onload = function () {
     // })();
 
   } else { //upload_activity.html일 때
-    //input 커스텀
-    let mtFile = document.getElementById('mt_file');
-    let studyFile = document.getElementById('study_file');
-    let projectFile = document.getElementById('project_file');
+      //input 커스텀
+      let mtFile = document.getElementById('mt');
+      let studyFile = document.getElementById('study');
+      let projectFile = document.getElementById('project');
 
-   // mt
-   mtFile.addEventListener('change', function (event) {
-    let fileName = event.target.files[0].name; // event 객체에서 선택된 파일의 이름을 가져옵니다.
-    document.getElementById('mt_upload_file_names').value = fileName; // 파일 이름을 입력 필드에 표시합니다.
-  });
+      // mt
+      mtFile.addEventListener('change', function (event) {
+        let fileName = event.target.files[0].name; // event 객체에서 선택된 파일의 이름을 가져옵니다.
+        document.getElementById('mt_file').value = fileName; // 파일 이름을 입력 필드에 표시합니다.
+      });
 
-  // study
-  studyFile.addEventListener('change', function (event) {
-    let fileName = event.target.files[0].name; // event 객체에서 선택된 파일의 이름을 가져옵니다.
-    document.getElementById('study_upload_file_names').value = fileName; // 파일 이름을 입력 필드에 표시합니다.
-  });
+      // study
+      studyFile.addEventListener('change', function (event) {
+        let fileName = event.target.files[0].name; // event 객체에서 선택된 파일의 이름을 가져옵니다.
+        document.getElementById('study_file').value = fileName; // 파일 이름을 입력 필드에 표시합니다.
+      });
 
-  // project
-  projectFile.addEventListener('change', function (event) {
-    let fileName = event.target.files[0].name; // event 객체에서 선택된 파일의 이름을 가져옵니다.
-    document.getElementById('project_upload_file_names').value = fileName; // 파일 이름을 입력 필드에 표시합니다.
-  });
-  
-
-
+      // project
+      projectFile.addEventListener('change', function (event) {
+        let fileName = event.target.files[0].name; // event 객체에서 선택된 파일의 이름을 가져옵니다.
+        document.getElementById('project_file').value = fileName; // 파일 이름을 입력 필드에 표시합니다.
+    });
   }
+  
 }
-
-
-
-
-
-
-
-
-
