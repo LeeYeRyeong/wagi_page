d5e915321374f9d45075fe032fcb11298f63cef3
s0obang <tnqls40168@gmail.com>
2024-01-16 02:55:44 +0900

    공지사항 사진추가시 표시

--- BACKEND/homepage/static/js/Notice_manager_writing.js
+++ BACKEND/homepage/static/js/Notice_manager_writing.js
@@ -13,3 +13,10 @@ function updateHiddenValue(button) {
     // 클릭된 버튼에 'selected' 클래스 추가
     button.classList.add('selected');
 }
+
+
+var fileName =  document.getElementById('profile'); // 파일명 추출
+fileName.addEventListener('change', function (event) {
+    let fileName = event.target.files[0].name; // event 객체에서 선택된 파일의 이름을 가져옵니다.
+    document.getElementById('selectedImageName').value = fileName; // 파일 이름을 입력 필드에 표시합니다.
+  });
