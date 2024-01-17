9b86ecd3a501ddd52f74459604c975808d027c41
maeng-kim <ehseo0410@gmail.com>
2024-01-17 11:56:30 +0900

    fix: logo수정

--- BACKEND/homepage/joinResult/views.py
+++ BACKEND/homepage/joinResult/views.py
@@ -8,8 +8,8 @@ from django.http import JsonResponse
 #  join 버튼 눌렀을 떄
 def join_button(request):
     current_date = date.today() # 현재 날짜
-    start_j_date = date(2024, 1, 1) # 지원서 제출 시작 날짜
-    end_j_date = date(2024, 1, 17) # 지원서 제 출 끝나는 날짜
+    start_j_date = date(2024, 3, 1) # 지원서 제출 시작 날짜
+    end_j_date = date(2024, 3, 17) # 지원서 제 출 끝나는 날짜
     start_r_date = date(2024, 1, 18) # 합격자 조회 시작 날짜
     end_r_date = date(2024, 1, 19) # 합격자 조회 끝나는 날짜
     isDate = 0
