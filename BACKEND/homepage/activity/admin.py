8d800432f7248ca1e5458a29d80ac0f77084532e
maeng-kim <ehseo0410@gmail.com>
2024-01-17 23:21:51 +0900

    commmit X

--- BACKEND/homepage/activity/admin.py
+++ BACKEND/homepage/activity/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Activity_mt, Activity_project, Activity_study
 
 # Register your models here.
+admin.site.register(Activity_mt)
+admin.site.register(Activity_project)
+admin.site.register(Activity_study)
