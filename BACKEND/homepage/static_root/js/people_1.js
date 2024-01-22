cd0000f260cd5aa44fb9f2d9de76cb010a3331e6
sujin <csujin2040@gmail.com>
2024-01-22 22:00:29 +0900

    static_root 추가

--- BACKEND/homepage/static_root/js/people_1.js
+++ BACKEND/homepage/static_root/js/people_1.js
@@ -110,6 +110,7 @@ document.addEventListener('DOMContentLoaded', function() {
         photo: "../static/img/people/전민선.png",
         bio: "즐거운 코딩 ~.~",
         link_1: "https://github.com/mminnn28",
+        link_2: "https://mminnn28.tistory.com/",
       },
       {
         name: "김지희",
@@ -127,17 +128,18 @@ document.addEventListener('DOMContentLoaded', function() {
       {
         name: "김다예",
         photo: "../static/img/people/김다예.png",
-        bio: "성신 최고 개발 동아리 WAGI 부원입니다 :D",
+        bio: "WAGI 1기 부원입니다. :D",
         link_1: "https://github.com/kdy09687",
       },
       
     ];
 
-    const container = document.querySelector('.center');
-    let visibleBubbles = 2;
+    //모바일일때 js
+    if (window.matchMedia('(max-width: 450px)').matches){
+      const container = document.querySelector('.center');
+      let visibleBubbles = 3;
 
-  
-    function createBubble(person, isLeft) {
+      function createBubble(person, isLeft) {
       const bubble = document.createElement('div');
       bubble.classList.add(isLeft ? 'L_bubble' : 'R_bubble');
   
@@ -148,8 +150,8 @@ document.addEventListener('DOMContentLoaded', function() {
       const img = document.createElement('img');
       img.src = person.photo;
       img.alt = person.name;
-      img.style.maxHeight = "120px";
-      img.style.maxWidth = "120px";
+      img.style.maxHeight = "95px";
+      img.style.maxWidth = "95px";
       photo.appendChild(img);
   
       const name = document.createElement('div');
@@ -157,19 +159,21 @@ document.addEventListener('DOMContentLoaded', function() {
       name.innerHTML = `<h2>${person.name}</h2>`;
   
       const rink = document.createElement('div');
-      rink.classList.add(isLeft ? 'L_rink' : 'R_rink');
-      //링크가 2개일때랑 1개일때 구분해서 생성하는 코드입니다. 
+      rink.classList.add(isLeft ? 'L_link' : 'R_link');
+      //링크가 2개일때랑 1개일때 구분해서 생성하는 코드
       if (person.link_2) {
         rink.innerHTML = `
-            <div>
-                <a href="${person.link_1}"><img src ="../static/img/github.svg" /></a>
-                <a href="${person.link_2}"><img src ="../static/img/home.svg" /></a>
-            </div>`;
-    } else {
-        rink.innerHTML = `<a href="${person.link_1}"><img src ="../static/img/github.svg" /></a>`;
-    }
+                <a href="${person.link_1}" style = "margin-left : 5px;"><img src ="../static/img/github_mini.svg" /></a>
+                <a href="${person.link_2}"  style = "margin-right : 5px;"><img src ="../static/img/home_mini.svg" /></a>
+      `;
+      } 
+      else {
+        rink.innerHTML = `
+          <a href="${person.link_1}"  style = " margin-left: 5px; float : "right;"
+          ><img src ="../static/img/github_mini.svg" /></a>`;
+      }
     
-  
+
       const text = document.createElement('div');
       text.classList.add(isLeft ? 'L_text' : 'R_text');
       text.innerHTML = `<p>"${person.bio}"</p>`;
@@ -180,22 +184,100 @@ document.addEventListener('DOMContentLoaded', function() {
       bubble.appendChild(text);
   
       container.appendChild(bubble);
-    }
-  
-    for (let i = 0; i < 2 && i < peopleData.length; i++) {
-      const person = peopleData[i];
-      createBubble(person, i % 2 === 0);    
-    }
-  
-
-    window.addEventListener('wheel', function (event) {
-      const deltaY = event.deltaY;
-      
-      if (deltaY > 60 && visibleBubbles < peopleData.length) {
-        const person = peopleData[visibleBubbles];
-        createBubble(person, visibleBubbles % 2 === 0);
-        visibleBubbles++;
       }
-    });
+  
+      for (let i = 0; i < 3 && i < peopleData.length; i++) {
+        const person = peopleData[i];
+        createBubble(person, i % 2 === 0);    
+      }
+
+    
+      var Y = 100;
+  
+      window.addEventListener('scroll', function () {
+        var winY = window.scrollY;
+        if (winY > Y*1.2) {
+          const person = peopleData[visibleBubbles];
+          createBubble(person, visibleBubbles % 2 === 0);
+          visibleBubbles++;
+         Y = Y + 100;
+        }
+      });
+
+    }
+
+    //pc 일때 js
+    else  {
+        const container = document.querySelector('.center');
+        let visibleBubbles = 3;
+
+        function createBubble(person, isLeft) {
+        const bubble = document.createElement('div');
+        bubble.classList.add(isLeft ? 'L_bubble' : 'R_bubble');
+    
+        bubble.style.animation = 'bubbleFadeIn 0.7s forwards';
+    
+        const photo = document.createElement('div');
+        photo.classList.add(isLeft ? 'L_photo' : 'R_photo');
+        const img = document.createElement('img');
+        img.src = person.photo;
+        img.alt = person.name;
+        img.style.maxHeight = "95px";
+        img.style.maxWidth = "95px";
+        photo.appendChild(img);
+    
+        const name = document.createElement('div');
+        name.classList.add(isLeft ? 'L_name' : 'R_name');
+        name.innerHTML = `<h2>${person.name}</h2>`;
+    
+        const rink = document.createElement('div');
+        rink.classList.add(isLeft ? 'L_rink' : 'R_rink');
+        //링크가 2개일때랑 1개일때 구분해서 생성하는 코드 
+        if (person.link_2) {
+          rink.innerHTML = `
+                  <a href="${person.link_1}" style = "margin-left : 5px;"><img src ="../static/img/github.svg" /></a>
+                  <a href="${person.link_2}"><img src ="../static/img/home.svg" /></a>
+        `;
+        } 
+        else {
+          rink.innerHTML = `
+            <a href="${person.link_1}"  style = "margin-left : 5px;"><img src ="../static/img/github.svg" /></a>`;
+        }
+      
+
+        const text = document.createElement('div');
+        text.classList.add(isLeft ? 'L_text' : 'R_text');
+        text.innerHTML = `<p>"${person.bio}"</p>`;
+    
+        bubble.appendChild(photo);
+        bubble.appendChild(name);
+        bubble.appendChild(rink);
+        bubble.appendChild(text);
+    
+        container.appendChild(bubble);
+      }
+    
+      for (let i = 0; i < 3 && i < peopleData.length; i++) {
+        const person = peopleData[i];
+        createBubble(person, i % 2 === 0);    
+      }
+
+      
+      var Y = 100;
+    
+      window.addEventListener('scroll', function () {
+        var winY = window.scrollY;
+        if (winY > Y*1.5) {
+          const person = peopleData[visibleBubbles];
+          createBubble(person, visibleBubbles % 2 === 0);
+          visibleBubbles++;
+          Y = Y + 140;
+        }
+      });
+
+    }
+
+
+
 
   });
