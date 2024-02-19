7512b867efb5c71fca27b2371d77af11942311d7
s0obang <109025445+s0obang@users.noreply.github.com>
2024-02-20 04:53:18 +0900

    Update admin.py

--- BACKEND/homepage/notice/admin.py
+++ BACKEND/homepage/notice/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Notion,NotionImage
+from .models import Notion,NotionImage, NotionFile
 
 admin.site.register(Notion)
 admin.site.register(NotionImage)
