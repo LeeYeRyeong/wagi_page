7cd4e4805310006112aa7a9c1e040b5a0f859271
s0obang <109025445+s0obang@users.noreply.github.com>
2024-02-20 00:11:31 +0900

    Update basic_modal.js

--- BACKEND/homepage/static_root/js/basic_modal.js
+++ BACKEND/homepage/static_root/js/basic_modal.js
@@ -1,7 +1,18 @@
 $(document).ready(function () {
-    $("#apply, #apply_").click(function (e) {
+    
+
+    $("#apply, #apply_").click(function (e){
         e.preventDefault();
         showModal();
+        const closeModal = document.querySelector('#close-modal'); // 모달 닫기 버튼 
+        var input = document.querySelector('.input_email');
+        closeModal.addEventListener("click", ()=>{
+            $(".modal_unavailable").hide();
+            input.value=null; //이메일 입력하다가 모달창 닫았을 때 input창 리셋
+            // 모달창 닫으면 메인페이지로 돌아감
+            window.location.href = '/';
+        });      
+        
     });
 
     $("#input_email").on('keyup', function (e) {
@@ -13,10 +24,12 @@ $(document).ready(function () {
     });
 
     $(document).on('click touchstart', '#close-modal', function () {
-        handleModalClose();
+        $('.message_2').css('opacity', '100');
+        $('.email').css('display', 'flex');
+        $('.out_text').removeClass('on');
     });
-
-    function showModal() {
+    
+    function showModal(){
         $.ajax({
             url: '/joinResult/join_button/',
             method: "GET",
@@ -28,6 +41,7 @@ $(document).ready(function () {
             success: function (data) {
                 if (data.result === 'show') {
                     $(".modal_unavailable").show();
+                    /*모바일버전 네비바 닫기*/
                     $("#burgur").removeClass('on');
                     $("#slide").removeClass('on');
                 }
@@ -36,24 +50,16 @@ $(document).ready(function () {
                 window.location.href = '/joinResult/join_button/';
             }
         });
-
-        const closeModal = document.querySelector('#close-modal');
-        var input = document.querySelector('.input_email');
-        closeModal.addEventListener("click", handleModalClose);
-    }
-
-    function handleModalClose() {
-        $(".modal_unavailable").hide();
-        var input = document.querySelector('.input_email');
-        input.value = null;
-        window.location.href = '/';
-    }
-
+    }   
+    
+    // 메일 입력 받기
+    var writeMailUrl =document.getElementById('email').getAttribute('action');
     $("#email").submit(function (event) {
         event.preventDefault();
         var userEmail = $(".input_email").val();
         $.ajax({
-            url: document.getElementById('email').getAttribute('action'),
+            url : writeMailUrl,
+
             method: "POST",
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
@@ -64,14 +70,19 @@ $(document).ready(function () {
                 'email': userEmail,
                 'csrfmiddlewaretoken': getCookie('csrftoken')
             },
+
             dataType: 'json',
             success: function (data) {
                 if (data.works) {
+                                
                     $( '.message_2' ).animate({ 
-                        opacity: 0
+                            opacity: 0
                     }, 100 , event.preventDefault());
+                        
                     $('.email').css('display', 'none');
                     $( '.out_text' ).addClass( 'on' );
+                        
+                                
                 } else if (data.noEmail) {
                     alert('이메일 주소를 입력해주세요.');
                 } else if (data.wrongEmail) {
@@ -85,25 +96,29 @@ $(document).ready(function () {
             }
         });
     });
+        
 });
 
+// Enter 키 이벤트 감지
 $("#input_email").keyup(function (e) {
-    if (e.keyCode === 13) {
+    if (e.keyCode === 13) { // Enter 키 코드
         e.preventDefault();
         var userEmail = $("#input_email").val();
         sendAjaxRequest(userEmail);
     }
 });
-
+// 이메일 제출 후 모달 창 닫았을 때 리셋
 $(document).ready(function() {
     $('#close-modal').on('click', function() {
+
         $('.message_2').css('opacity', '100');
         $('.email').css('display', 'flex');
         $( '.out_text' ).removeClass( 'on' );
     });
 });
 
-function getCookie(name) {
+ // CSRF 토큰을 쿠키에서 가져오는 함수
+ function getCookie(name) {
     var cookieValue = null;
     if (document.cookie && document.cookie !== '') {
         var cookies = document.cookie.split(';');
