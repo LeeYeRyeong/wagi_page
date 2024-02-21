bd1f9c82d709b6de54610fc9bf51be60299d7173
s0obang <tnqls40168@gmail.com>
2024-02-21 18:16:49 +0900

    중복 학번 지원 불가처리--이거 안하면 조회 기능 사용에 불편o

--- BACKEND/homepage/joinInfo/views.py
+++ BACKEND/homepage/joinInfo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect, get_object_or_404
 from django import forms
 from .models import JoinInfo
 from django.http import Http404
+from django.contrib import messages
 
 # Create your views here.
 
@@ -13,6 +14,13 @@ class ApplicantForm(forms.ModelForm):
 def apply_club_user_info_view(request):
     if request.method == 'POST':
         form = ApplicantForm(request.POST, request.FILES)
+        idcheck=request.POST.get('user_number')
+        #기존에 한번 지원한 친구들은 빠꾸맥이기~ 어쩔수 엇ㅂ어 안하면 오류나; 정신똑바로차리고살아야지이런각박한세상속에서
+        if JoinInfo.objects.filter(user_number=idcheck).exists():
+            error_message = '이미 지원 완료된 학번입니다.<br>관련 문의는 인스타그램 @sswu_wagi로 부탁드립니다.'
+            messages.error(request, error_message, extra_tags='safe')
+            return render(request, 'join_info.html', {'form': form})
+        
         if form.is_valid():
             app_sub=form.save()
             name = ''
